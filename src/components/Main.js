be022a1ca27acf83c0fae5f5632a26d071831cfe
fujin <fujin@vchangyi.com>
2018-04-30 21:30:25 +0800

    图片、总体样式

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,37 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
+
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// 获取图片相关的数据
+var imagesDatas = require('../data/imagesData.json')
+// let yeomanImage = require('../images/yeoman.png');
+
+// 利用自执行函数，将图片名称转成图片路径信息
+function genImageURL (imagesDatasArr) {
+	for(var i =0,j = imagesDatasArr.length; i < j; i++) {
+		var singleImageData = imagesDatasArr[i]
+
+		singleImageData.imageURL = require('../images/' + singleImageData[i].fileName)
+
+		imagesDatasArr[i] = singleImageData
+	}
+	return imagesDatasArr
+}
+imagesDatas = genImageURL(imagesDatas)
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <span>hello react</span>
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+      	<section className="img-sec">
+
+				</section>
+				<nav className="controller-nav">
+
+				</nav>
+      </section>
     );
   }
 }
